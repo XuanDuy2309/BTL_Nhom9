8dae1de380f953f189c68e02ed6eb05ecf9f3152
XuanDuy2309 <119564395+XuanDuy2309@users.noreply.github.com>
2024-03-12 12:38:11 +0700

    init project

---
+++ app/src/main/java/com/example/btl_nhom9/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.btl_nhom9.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.btl_nhom9.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
