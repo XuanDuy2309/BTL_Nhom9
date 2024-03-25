b4c05ebc648c627de8a445ffa4d481056e4bba5f
XuanDuy2309 <119564395+XuanDuy2309@users.noreply.github.com>
2024-03-25 11:05:05 +0700

    update project's Duy

---
+++ app/src/main/java/com/example/btl_nhom9/activities/AddressActivity.java
@@ -0,0 +1,16 @@
+package com.example.btl_nhom9.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.btl_nhom9.R;
+
+public class AddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+}
